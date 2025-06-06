8f3041e76c2a36d6a27d31aee617edb8e026b27b
PreLuc <pedromigueldealcantara3@gmail.com>
2025-06-06 16:08:41 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,62 +1,37 @@
-import csv
+print('ola mundo')
+print('eu era bom')
+print('salve zona sul')
 
-def obter_campos_personalizados():
-    print('quais informações sobre os times você deseja cadastrar? (ex:franquia,título,cidade)')
-    campos= input('separe os campos por vírgula:').split(',')
-    campos= [campo.strip() for campo in campos]
-    return campos
+caixa='celular'
 
-def cadastrar_times(campos):
-    times = []
-    
-    while True:
-         time = {}
-         for campo in campos:
-                valor = input(f'digite o valor para {campo}:')
-                time[campo]= valor
+print(caixa)
 
-         times.append(time)
+caixa='bike a sla'
 
-         continuar=input('deseja cadastrar outro time?(s/n):').lower()
-         if continuar !='s':
-                break
-    return times
-    
+print(caixa)
 
-def salvar_em_csv(times,campos,nome_arquivo='times.csv'):
-        try:
-            with open(nome_arquivo,mode='w',newline='', encording='utf-8') as arquivo:
-                writer = csv.DictWriter(arquivo, fieldnames=campos)
-                writer.writeheader()
-                writer.writerows(times)
-            print(f'/nDados salvos com sucesso em"{nome_arquivo}"')
-        except Exception as e:
-            print('erro ao salvar o arquivo')
-def exibir_dados_csv(nome_arquivo='alunos.csv'):
-                print('/n---Dados cadastrados---/n')
-                try:
-                    with open(nome_arquivo, mode='r',encoding='utf-8') as arquivo:
-                        reader=csv.DictReader(arquivo)
-                        for linha in reader:
-                         print(linha)
-                   
-                except FileNotFoundError:
-                    print('arquivo não encontrado.')
-                except Exception as e:
-                    print('erro ao ler arquivo', e)
+caixa='teclado'
 
-def main():
-      print('===sistema de cadastro de jogadores===')
+print(caixa)
 
-      campos= obter_campos_personalizados()
-      times=cadastrar_times(campos)
-      salvar_em_csv(times,campos)
-      exibir_dados_csv()
-if __name__=='__main__':
-           main()
+nome= 'pedro'
+idade=  16
+altura= 1.79
+estudante= True
+print(nome,idade,altura,estudante)
 
-            
+mensagem= 'estou ficando inteligente'
+print(mensagem.strip())
+print(mensagem.upper())
+print(mensagem.lower())
 
-        
-                
-    
+name=input('insira o seu nome!')
+print(f'ola {nome}, seja muito bem vindo ao meu site')
+
+from datetime import datetime
+
+nome= input('qual e o seu nome')
+agora= datetime.now()
+hora_atual=agora.strftime('%H:%M')
+print(f'ola, {nome}! Agora sao {hora_atual}')
+
